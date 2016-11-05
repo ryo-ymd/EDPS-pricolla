7e95b49e6b47fefaca286ae181497578b6061af2
明地えしら <ecilavip@outlook.jp>
2016-11-05 10:17:06 +0900

    [add] シャドバとデレステ

--- pricolla/config/initializers/assets.rb
+++ pricolla/config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( pk.scss )
 Rails.application.config.assets.precompile += %w( sv.scss )
 Rails.application.config.assets.precompile += %w( dq.scss )
+Rails.application.config.assets.precompile += %w( ds.scss )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path

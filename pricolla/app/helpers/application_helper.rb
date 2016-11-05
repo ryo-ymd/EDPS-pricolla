4cbb1474e00eb3a527c949a69c548cd2b02e86cc
明地えしら <ecilavip@outlook.jp>
2016-11-05 10:53:38 +0900

    [add] 見栄えを良くした

--- pricolla/app/helpers/application_helper.rb
+++ pricolla/app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def temp_type(type)
+    case type
+      when 0
+        'ポケモン風'
+      when 1
+        'ドラクエ風'
+      when 2
+        'シャドバ風'
+      when 3
+        'デレステ風'
+    end
+  end
 end
